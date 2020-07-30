4f3bbd8a5934d8ac185b402fb95d8102bbb19108
YasmimS <yasmim.hpforever@gmail.com>
2020-07-29 22:13:26 -0300

    Versão Inicial

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+         Imersão React da Alura
+        </a>
+      </p>
+      <p>by Yasmim Sancha © 2020</p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
